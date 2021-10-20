245f77322b22b060c30c985523c3160fae98efe5
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-10-20 10:56:36 -0300

    hello cargo

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-03878aafcff328b9.d
@@ -0,0 +1,5 @@
+/Users/bfrancisco/dev/rust/the_rust_programming_language/hello_cargo/target/rls/debug/deps/hello_cargo-03878aafcff328b9.rmeta: src/main.rs
+
+/Users/bfrancisco/dev/rust/the_rust_programming_language/hello_cargo/target/rls/debug/deps/hello_cargo-03878aafcff328b9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-4eb4045a891c904b.d
@@ -0,0 +1,5 @@
+/Users/bfrancisco/dev/rust/the_rust_programming_language/hello_cargo/target/rls/debug/deps/hello_cargo-4eb4045a891c904b.rmeta: src/main.rs
+
+/Users/bfrancisco/dev/rust/the_rust_programming_language/hello_cargo/target/rls/debug/deps/hello_cargo-4eb4045a891c904b.d: src/main.rs
+
+src/main.rs:
