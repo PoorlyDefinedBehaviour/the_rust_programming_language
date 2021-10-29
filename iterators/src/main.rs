a0aed312650a433ff5f0a411be0230e22c409756
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-10-29 19:27:03 -0300

    iterator adaptors

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 // The iterator pattern allows you to perform some task on a sequence
 // of items in turn. An iterator is responsible for the logic
 // of iterating over each item and determining when the
@@ -10,7 +12,7 @@
 // All iterators implement a trait Named Iterator that is defined by
 // the standard library.
 // It kinda looks like this:
-pub trait Iterator {
+pub trait MyIterator {
   // Item is an associated type
   type Item;
 
@@ -68,4 +70,111 @@ fn main() {
   // complete.
   let v1 = vec![1, 2, 3];
   assert_eq!(v1.iter().sum::<i32>(), 6);
+
+  // Methods that produce other iterators
+  //
+  // Other methods defined on the Iterator trait, known as iterator adaptors,
+  // allow you to change iterators into different kinds of iterators.
+  // You can chain multiple calls to iterator adaptors to perform
+  // complex actions in a readable way. But because all iterators are lazy,
+  // you have to call one of the consuming adaptor methods to get results
+  // from calls to iterator adaptors.
+  let v1 = vec![1, 2, 3];
+
+  dbg!(v1.iter().map(|x| x + 1).collect::<Vec<i32>>()); // [2, 3, 4]
+}
+
+struct Counter {
+  count: u32,
+}
+
+impl Counter {
+  fn new() -> Self {
+    Self { count: 0 }
+  }
+}
+
+impl Iterator for Counter {
+  type Item = u32;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.count < 5 {
+      self.count += 1;
+      Some(self.count)
+    } else {
+      None
+    }
+  }
+}
+
+#[derive(Debug, PartialEq)]
+struct Shoe {
+  size: u32,
+  style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+  shoes
+    .into_iter()
+    .filter(|shoe| shoe.size == shoe_size)
+    .collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+  }
+
+  #[test]
+  fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+      .zip(Counter::new().skip(1))
+      .map(|(a, b)| a * b)
+      .filter(|x| x % 3 == 0)
+      .sum();
+
+    assert_eq!(18, sum);
+  }
+
+  #[test]
+  fn filters_by_size() {
+    let shoes = vec![
+      Shoe {
+        size: 10,
+        style: String::from("sneaker"),
+      },
+      Shoe {
+        size: 13,
+        style: String::from("sandal"),
+      },
+      Shoe {
+        size: 10,
+        style: String::from("boot"),
+      },
+    ];
+
+    let expected = vec![
+      Shoe {
+        size: 10,
+        style: String::from("sneaker"),
+      },
+      Shoe {
+        size: 10,
+        style: String::from("boot"),
+      },
+    ];
+
+    assert_eq!(shoes_in_size(shoes, 10), expected);
+  }
 }
