a57a1fdd189f310b6fb04b20514192df4aeabc74
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-11-02 17:25:15 -0300

    Fearless Concurrency

---
+++ fearless_concurrency/src/main.rs
@@ -0,0 +1,11 @@
+// Fearless Concurrency
+//
+// Concurrent programming: where different parts of a program
+// execute independently.
+//
+// Paralel programming: where different parts of a program
+// execute at the same time.
+
+fn main() {
+  println!("Hello, world!");
+}
